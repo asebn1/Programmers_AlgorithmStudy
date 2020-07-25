03c4db2caa4ea256251d915d486e2d8e1742f90e
DESKTOP-71EH2PM\공재호 <asebn1@khu.ac.kr>
2020-07-26 02:38:54 +0900

    solved

--- 완주하지 못한 선수.py
+++ 완주하지 못한 선수.py
@@ -1,9 +1,10 @@
 
 import collections
 
-# collections 사용하면 - 사용 가능
 def solution(participant, completion):
+    # collections 사용하면 - 사용 가능
     answer = collections.Counter(participant) - collections.Counter(completion)
+    # 딕셔너리 형태를 리스트로 변환 후 출력
     return list(answer.keys())[0]
 
 print(solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"]))
